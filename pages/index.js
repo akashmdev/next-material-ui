94f83ffe8dba5278be8808b14e35d608c804e4ae
1930001 <abhishard1215@gmail.com>
2021-12-23 18:09:33 +0530

    initialchange

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,20 @@ import Box from '@mui/material/Box';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
+import Button from "@mui/material/Button";
+import {Avatar, TextField} from "@mui/material";
+import {deepPurple} from "@mui/material/colors";
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
+    <Container  maxWidth={'sm'}>
+      <Box sx={{ my: 2 }}>
+        <Avatar sx={{ width: 75, height:75,bgcolor: deepPurple[500] }}>AS</Avatar>
+          <p>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </p>
         <Link href="/about" color="secondary">
-          Go to the about page
+           hhhhhh
         </Link>
         <ProTip />
         <Copyright />
